90cda45ebe855504cec181560d3335b424aafb51
David O'Morrissey <domorrissey@gmail.com>
2018-04-25 15:56:54 -0400

    Visual Spacing

--- static/js/chart.js
+++ static/js/chart.js
@@ -6,16 +6,16 @@ function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     /* Add the Script value here - Might need a new route on app.py */
-    ['Rate', 37],
+    ['Rate', 32.124000000001],
   ]);
 
   var options = {
     width: 600, height: 540,
-    redFrom: 27, redTo: 50,
-    yellowFrom:13, yellowTo: 27,
-    greenFrom: 0, greenTo: 13,
-    min: 0, max: 50,
-    minorTicks: 0, majorTicks: 4
+    redFrom: 25, redTo: 50,
+    yellowFrom:12, yellowTo: 25,
+    greenFrom: 0, greenTo: 12,
+    min: 0, max: 40,
+    minorTicks: 0, majorTicks: 3
   };
 
   var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
