ad909a65f832f67a5b92a30d48b7845dbb4bc371
KKopacz1 <Kyle.Kopacz@gmail.com>
2018-04-26 18:56:40 -0400

    Tach update

--- static/js/chart.js
+++ static/js/chart.js
@@ -6,16 +6,16 @@ function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
     /* Add the Script value here - Might need a new route on app.py */
-    ['Rate', 32.124000000001],
+    ['Rate', 32.0],
   ]);
 
   var options = {
     width: 600, height: 540,
-    redFrom: 25, redTo: 50,
-    yellowFrom:12, yellowTo: 25,
-    greenFrom: 0, greenTo: 12,
+    redFrom: 25, redTo: 40,
+    yellowFrom:10, yellowTo: 25,
+    greenFrom: 0, greenTo: 10,
     min: 0, max: 40,
-    minorTicks: 0, majorTicks: 3
+    minorTicks: 4, majorTicks: 3
   };
 
   var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
